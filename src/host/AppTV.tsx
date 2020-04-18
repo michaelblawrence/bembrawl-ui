a87833f910c8140e2fa0b752d7e74d42dc2a76ed
Joshua Mason <joshua.mark.mason95@gmail.com>
2020-04-18 21:07:54 +0100

    begin editing welcome page

--- src/host/AppTV.tsx
+++ src/host/AppTV.tsx
@@ -1,19 +1,44 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
+import { PageState } from "./core/enums/PageState";
 import "./AppTV.css";
+import { WaitingForUsers } from "./core-features/WaitingForUsers/WaitingForUsers";
 
 function AppTV() {
-  // const [page, setPage] = useState(PageState.PlayersAnswerReview);
-  // useFullScreen(page);
-  // useServerPageFSM(page, setPage);
+  const [page, setPage] = useState(PageState.WaitingForUsers);
+  useServerPageFSM(page, setPage);
   return (
     <div className="Window">
-      {/* {page === PageState.JoinRoom && <JoinPage setPage={setPage} />}
-      {page === PageState.WaitingRoom && <WaitingRoomPage setPage={setPage} />}
+      {page === PageState.WaitingForUsers && <WaitingForUsers setPage={setPage} />}
+      {/* {page === PageState.QuestionAndAnswers && <WaitingRoomPage setPage={setPage} />}
       {page === PageState.PlayersAnswer && <PlayersAnswerPage setPage={setPage} />}
       {page === PageState.PlayersAnswerReview && <PlayersAnswerReviewPage setPage={setPage} />} */}
     </div>
   );
 }
+
+
+function useServerPageFSM(
+  page: PageState,
+  setPage: React.Dispatch<React.SetStateAction<PageState>>
+) {
+  // TODO: interact with server here
+  useEffect(() => {
+    let handle: number | null = null;
+    // if (page === PageState.WaitingRoom) {
+    //   const timeoutHandle: any = setTimeout(
+    //     () => setPage(PageState.PlayersAnswer),
+    //     1000
+    //   );
+    //   handle = timeoutHandle as number;
+    // }
+    return () => {
+      if (handle) {
+        clearInterval(handle);
+      }
+    };
+  }, [page, setPage]);
+}
+
 
 export default AppTV;
---
+++ src/host/core/enums/PageState.tsx
@@ -0,0 +1,6 @@
+export enum PageState {
+  WaitingForUsers,
+  QuestionAndAnswers,
+  QuestionResults,
+  FinalResults
+}
