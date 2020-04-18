69f511a991a1888c6faae041267471db7596e228
Joshua Mason <joshua.mark.mason95@gmail.com>
2020-04-18 20:23:27 +0100

    split tv and mobile routing

---
+++ src/host/AppTV.tsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from "react";
+import { useState } from "react";
+import "./AppTV.css";
+
+function AppTV() {
+  // const [page, setPage] = useState(PageState.PlayersAnswerReview);
+  // useFullScreen(page);
+  // useServerPageFSM(page, setPage);
+  return (
+    <div className="Window">
+      {/* {page === PageState.JoinRoom && <JoinPage setPage={setPage} />}
+      {page === PageState.WaitingRoom && <WaitingRoomPage setPage={setPage} />}
+      {page === PageState.PlayersAnswer && <PlayersAnswerPage setPage={setPage} />}
+      {page === PageState.PlayersAnswerReview && <PlayersAnswerReviewPage setPage={setPage} />} */}
+    </div>
+  );
+}
+
+export default AppTV;
