2dee94c2dee012d52e228193f9905011b2747f3b
Joshua Mason <joshua.mark.mason95@gmail.com>
2020-04-27 22:36:56 +0100

    removal of unnecessary code

--- src/mobile/features/PlayersAnswerPage/PlayersAnswerPage.tsx
+++ src/mobile/features/PlayersAnswerPage/PlayersAnswerPage.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useRef, useEffect } from "react";
-import emojiRegex from "emoji-regex";
 import "./PlayersAnswerPage.css";
 import { Branding } from "../../../core-common/Branding";
 import { Grid, TextField, Button } from "@material-ui/core";
@@ -42,11 +41,6 @@ function QuestionSection(props: { emojiCount: number; songTitle: string }) {
   );
 }
 
-const isEmoji = (text: string) => {
-  const matches = emojiRegex().exec(text);
-  return matches && matches[0];
-};
-
 function EmojiAnswerSlots(props: {
   emojiCount: number;
   onSubmit: OnEmojiSubmit;
@@ -73,35 +67,21 @@ function EmojiAnswerSlots(props: {
 
   const [emojiIndex, setEmojiIndex] = useState<number>(0);
 
-  const onSlotChange = (idx: number) => (
-    e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
-  ) => {
-    console.log(idx);
-    const text = (e && e.target && e.target.value) || "";
-    handleSlotChange(text, idx, emojiCount, slotRefs, slotState);
-  };
-
   const onSlotSelect = (idx: number) => (
-    e: React.SyntheticEvent<HTMLDivElement, Event>    
+    e: React.SyntheticEvent<HTMLDivElement, Event>
   ) => {
     setEmojiIndex(idx);
-    console.log(idx);
-    console.log(89);
-    // setEmojiIndex(idx);
-    // const text = (e && e.target && e.target.value) || "";
-    // handleSlotChange(text, idx, emojiCount, slotRefs, slotState);
   };
 
   const slots = new Array(emojiCount).fill(0).map((_, idx) => (
     <Grid item xs={2} key={idx}>
-      <TextField
+      <TextField // TODO stop keyboard showing up when text box clicked
         fullWidth={true}
         ref={slotRefs[idx]}
-        onChange={onSlotChange(idx)}
+        // onChange={onSlotChange(idx)}
         onSelect={onSlotSelect(idx)}
         inputProps={{ inputMode: "search", style: { textAlign: "center" } }}
         value={slotState[idx][0]}
-        // autoFocus={idx === 0}
       />
     </Grid>
   ));
@@ -113,49 +93,25 @@ function EmojiAnswerSlots(props: {
   const onSubmitClick = () => onSubmit(typedEmoji());
   useEffect(() => {
     const incomplete = typedEmoji().some((text) => !text);
-    console.log("changed", slotState, "incomplete", incomplete);
     setIsIncomplete(incomplete);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [slotState]);
 
-  // const onEmojiSelect = (emojiData: BaseEmoji) => {
-  //   slotState[emojiIndex][1](emojiData.native || "");
-  //   setEmojiIndex(emojiIndex + 1);
-  // };
-  
   const onEmojiClick = (emojiData: BaseEmoji) => {
-    console.log(`set index ${emojiIndex}`)
     slotState[emojiIndex][1](emojiData.native || "");
+    const nextEmptyN = !slotState[emojiIndex][0] ? true : false;
+    let nextIdx = slotState.findIndex(([slot]) => !slot);
 
-    const nextEmptyN = !emojiIndex ? emojiIndex + 1 : emojiIndex;
+    if (nextEmptyN) {
+      nextIdx += slotState.slice(nextIdx + 2).findIndex(([slot]) => !slot) + 1;
+    }
 
-    console.log(nextEmptyN);
-
-    const nextIdx = slotState.findIndex(([slot]) => !slot);
-
-
-    
-    console.log(nextIdx)
-    if (nextIdx >=0 && nextIdx < slotState.length - 1) {
-
-      console.log("131")
+    if (nextIdx >= 0 && nextIdx < slotState.length - 1) {
       setEmojiIndex(nextIdx);
-      console.log(`next index ${emojiIndex}`)
-
-    }  
-    else {
+    } else {
       setEmojiIndex(slotState.length - 2);
     }
-    console.log(emojiIndex)
 
-
-    // slotState.forEach(([slot, setSlot], idx) => {
-    //   if (slot === "") {
-    //     console.log(idx)
-    //     setEmojiIndex(idx)
-    //   }
-    // });
-    // setEmojiIndex(emojiIndex + 1);
   };
 
   return (
@@ -178,39 +134,8 @@ function EmojiAnswerSlots(props: {
         emoji="point_up"
         showSkinTones={false} // both of these required
         showPreview={false} // to remove bottom tab
-        // onSelect={onEmojiSelect}
         onClick={onEmojiClick}
       />
     </div>
   );
-}
-
-function handleSlotChange(
-  text: string,
-  idx: number,
-  totalCount: number,
-  slotRefs: React.RefObject<HTMLDivElement>[],
-  slotState: [string, React.Dispatch<React.SetStateAction<string>>][]
-) {
-  if (text !== "" && (!text || !isEmoji(text))) {
-    return;
-  }
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [_, setSlotText] = slotState[idx];
-  setSlotText(text);
-
-  const nextIdx = text === "" ? idx : idx + 1;
-  const nextRef =
-    nextIdx < totalCount && slotRefs[nextIdx] && slotRefs[nextIdx].current;
-
-  if (nextRef) {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const [_, setNextSlotText] = slotState[nextIdx];
-    setNextSlotText("");
-    nextRef.focus();
-    const input = nextRef.querySelector("input");
-    if (input) {
-      input.focus();
-    }
-  }
 }
