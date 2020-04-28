c7ccb9dd08bf91214d97db8db972f7b49250f7d2
Joshua Mason <joshua.mark.mason95@gmail.com>
2020-04-28 18:32:11 +0100

    uncomment server requests

--- src/mobile/effects/useServerPageFSM.tsx
+++ src/mobile/effects/useServerPageFSM.tsx
@@ -51,5 +51,5 @@ function mapServiceToMessages(svc: HostClientService | null): Messages {
 
 async function startHostClientService(hostClientService: HostClientService) {
   if (!hostClientService) return;
-  // await hostClientService.connect();
+  await hostClientService.connect();
 }
